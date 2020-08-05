7b7c3679dacb051d603806f2a6cfac0603fc9c74
larapollehn <lara.pollehn@googlemail.com>
2020-08-05 14:54:03 +0200

    Strict null check

---
+++ main.ts
@@ -0,0 +1,43 @@
+import NeedlemanWunschSimilarity from "./src/algorithms/NeedlemanWunschSimilarity";
+import AligningAlgorithm from "./src/algorithms/AligningAlgorithm";
+import TextProducer from "./src/text/TextProducer";
+import SimpleTextProducer from "./src/text/SimpleTextProducer";
+import NeedlemanWunschDistance from "./src/algorithms/NeedlemanWunschDistance";
+import SmithWaterman from "./src/algorithms/SmithWaterman";
+
+
+const similarity : AligningAlgorithm = new NeedlemanWunschSimilarity("ATCCTC", "AACG", 1, -1, -2);
+const distance: AligningAlgorithm = new NeedlemanWunschDistance("ATCCTC", "AACG", -1, 1, 2)
+
+const similarityTextProducer: TextProducer = new SimpleTextProducer(similarity);
+const similarityTexts = similarityTextProducer.produceText();
+const distanceTextProducer: TextProducer = new SimpleTextProducer(distance);
+const distanceTexts = distanceTextProducer.produceText();
+/**
+ for(let i = 0; i < similarityTexts.length; i++){
+    console.log(similarityTexts[i]);
+}
+ console.log("###########################");
+ for(let i = 0; i < distanceTexts.length; i++){
+    console.log(distanceTexts[i]);
+}
+ **/
+
+const smithWaterman: AligningAlgorithm = new SmithWaterman("ATCGAAT", "AACGTA",1, -1, -2);
+const paths = smithWaterman.align();
+
+/**
+ for (let i = 0; i < paths.length; i++){
+    for (let j = 0; j < paths[i].length; j++){
+        console.log(`${paths[i][j].x_position} | ${paths[i][j].y_position}`);
+    }
+    console.log('#####');
+}
+ **/
+
+const smithWatermanTextProducer: TextProducer = new SimpleTextProducer(smithWaterman);
+const smithWatermanTexts = smithWatermanTextProducer.produceText();
+
+for(let i = 0; i < smithWatermanTexts.length; i++){
+    console.log(smithWatermanTexts[i]);
+}
