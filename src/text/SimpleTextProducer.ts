4e4b7d1a43eed1e208f452e7cfec70c092fc2c92
larapollehn <lara.pollehn@googlemail.com>
2020-08-05 14:33:55 +0200

    changed TextProducer to fit more than one algorithm

--- src/text/SimpleTextProducer.ts
+++ src/text/SimpleTextProducer.ts
@@ -17,33 +17,25 @@ export default class SimpleTextProducer extends TextProducer {
         const mismatch = '|';
         const chain = path.reverse();
 
-        // markers that repr. indices
-        let a_marker = 0;
-        let b_marker = 0;
-
         let result = ['', '', '']; // result shows both sequences and the visual rep of their match/mismatch/gap
         for (let i = 0; i < chain.length - 1; i++) {
             if (chain[i].x_position + 1 === chain[i + 1].x_position && chain[i].y_position + 1 === chain[i + 1].y_position) {
-                if (this.aligningAlgorithm.sequence_a[a_marker] === this.aligningAlgorithm.sequence_b[b_marker]) {
+                if (this.aligningAlgorithm.sequence_a[i] === this.aligningAlgorithm.sequence_b[i]) {
                     result[1] += match;
                 } else {
                     result[1] += mismatch;
                 }
-                result[0] += chain[i].x_value;
-                result[2] += chain[i].y_value;
+                result[0] += chain[i+1].y_value;
+                result[2] += chain[i+1].x_value;
             } else if (chain[i].x_position === chain[i + 1].x_position) {
-                result[0] += chain[i].x_value;
+                result[0] += chain[i+1].y_value;
                 result[1] += ' ';
                 result[2] += gap;
-                b_marker--; //decrement index, because current seq value is skipped and marker is incremented after that
             } else if (chain[i].y_position === chain[i].y_position) {
                 result[0] += gap;
-                a_marker--; //decrement index, because current seq value is skipped and marker is incremented after that
                 result[1] += ' ';
-                result[2] += chain[i].y_value;
+                result[2] += chain[i+1].x_value;
             }
-            a_marker++;
-            b_marker++;
         }
         return result;
     }
