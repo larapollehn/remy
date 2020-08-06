5753aab1707f9f1af74ad55b73afdbe57966c625
larapollehn <lara.pollehn@googlemail.com>
2020-08-06 18:14:51 +0200

    added the alignment paths, also displayed them

--- src/components/Matrix.tsx
+++ src/components/Matrix.tsx
@@ -3,6 +3,7 @@ import NeedlemanWunschSimilarity from "../algorithms/NeedlemanWunschSimilarity";
 import NeedlemanWunschDistance from "../algorithms/NeedlemanWunschDistance";
 import SmithWaterman from "../algorithms/SmithWaterman";
 import Cell from "../algorithms/Cell";
+import SimpleTextProducer from "../text/SimpleTextProducer";
 
 interface Props {
     algorithm: string,
@@ -16,20 +17,26 @@ interface Props {
 const Matrix = (props: Props) => {
     const {algorithm, seqA, seqB, matchScore, mismatchScore, gapScore} = props;
     const [matrix, setMatrix] = useState([]);
+    const [paths, setPaths] = useState([]);
 
     const createMatrix = () => {
         console.log(seqA, seqB, matchScore, mismatchScore, gapScore);
         if (algorithm === "NeedelemanWunschSimilarity") {
             const needlemanWunschSimilarity = new NeedlemanWunschSimilarity(seqA, seqB, matchScore, mismatchScore, gapScore);
+            const similarityTextProducer = new SimpleTextProducer(needlemanWunschSimilarity);
             setMatrix(needlemanWunschSimilarity.matrix);
+            setPaths(similarityTextProducer.produceText());
         } else if (algorithm === "NeedlemanWunschDistance") {
             const needlemanWunschDistance = new NeedlemanWunschDistance(seqA, seqB, matchScore, mismatchScore, gapScore);
+            const distanceTextProducer = new SimpleTextProducer(needlemanWunschDistance);
             setMatrix(needlemanWunschDistance.matrix);
+            setPaths(distanceTextProducer.produceText());
         } else if (algorithm === "SmithWaterman") {
             const smithWaterman = new SmithWaterman(seqA, seqB, matchScore, mismatchScore, gapScore);
+            const smithWatermanTextProducer = new SimpleTextProducer(smithWaterman);
             setMatrix(smithWaterman.matrix);
+            setPaths(smithWatermanTextProducer.produceText());
         }
-        console.log(matrix);
     }
 
     useEffect(() => {
@@ -40,15 +47,23 @@ const Matrix = (props: Props) => {
         <div>
             <h2>{algorithm}</h2>
             <table>
-                {matrix.map((elem) => (
-                    <tr>
+                <tbody>
+                {matrix.map((elem, j) => (
+                    <tr key={j}>
                         {elem.map((cell: Cell, i: number) => (
                             <th key={i} style={{minWidth: "30px"}}>{cell.final_score}</th>
                         ))}
                     </tr>
                 ))}
+                </tbody>
             </table>
-
+            {paths.map((path: string[], i: number) => (
+                <ul key={i}>
+                    {path.map((text: string, j: number) => (
+                        <li key={j}>{text}</li>
+                    ))}
+                </ul>
+            ))}
         </div>
     )
 }
