a0e654eaca80945bfe05bc95d78816234e04b702
larapollehn <lara.pollehn@googlemail.com>
2020-08-09 15:31:37 +0200

    refactored use of algorithm with global maps

--- src/Globals.tsx
+++ src/Globals.tsx
@@ -4,11 +4,18 @@ import SimpleTextProducer from "./text/SimpleTextProducer";
 import NeedlemanWunschDistance from "./algorithms/NeedlemanWunschDistance";
 import SmithWaterman from "./algorithms/SmithWaterman";
 
+/*
+* used for the Select/Options in Settings.tsx
+ */
 const needlemanWunschDistance = "Needleman-Wunsch Distance";
 const needlemanWunschSimilarity = "Needleman-Wunsch Similarity";
 const smithWaterman = "Smith-Waterman";
 export const algorithmNames = [needlemanWunschSimilarity, needlemanWunschDistance, smithWaterman];
 
+/*
+* used in function setupMatrix in Matrix.tsx
+* mapping helps cut down on if/else when new algorithm is added
+ */
 const setupNeedleManWunschDistance = (seqA: string, seqB: string, matchScore: number, mismatchScore: number, gapScore: number) => {
     const needlemanWunschDistance = new NeedlemanWunschDistance(seqA, seqB, matchScore, mismatchScore, gapScore);
     const distanceTextProducer = new SimpleTextProducer(needlemanWunschDistance);
@@ -31,6 +38,12 @@ for(let i = 0; i < algorithmNames.length; i++){
     setupFunction.set(algorithmNames[i], algorithmFunctions[i]);
 }
 
+
+/*
+ * used for function handleInputChange in file Settings.tsx
+ * needed to stop the program from crashing when choosing Needleman-Wunsch Distance with bad score parameters
+ * it would calculate dozens of paths and freeze my laptop
+ */
 const needlemanWunschSimilarityScores = {match: 1, mismatch: -1, gap: -2};
 const needlemanWunschDistanceScores = {match: -1, mismatch: 1, gap: 2};
 const smithWatermanScores = {match: 1, mismatch: -1, gap: -2};
